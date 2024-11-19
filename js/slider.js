5038f31b133c6cd3f318176691eac54d4785dbd5
firespark <fireleit@gmail.com>
2024-11-20 00:26:05 +0400

    feat: add sliders and gallery

---
+++ js/slider.js
@@ -0,0 +1,67 @@
+const reviewsSlider = new Swiper('.reviews-slider', {
+    // Optional parameters
+    loop: true,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.slider-nav.right',
+      prevEl: '.slider-nav.left',
+    },
+  
+  });
+  
+  const productSlider = new Swiper('.product-slides', {
+    // Optional parameters
+    loop: true,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.product-slider-nav.right',
+      prevEl: '.product-slider-nav.left',
+    },
+  
+  });
+  
+  
+  const optionsSlider = new Swiper('.options-slides', {
+    // Optional parameters
+    loop: true,
+    slidesPerGroup: 1,
+    slidesPerView: 3,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.options-slider-nav.right',
+      prevEl: '.options-slider-nav.left',
+    },
+  
+  });
+  
+  const rewardsSlider = new Swiper('.rewards-slides', {
+    // Optional parameters
+    loop: true,
+    slidesPerGroup: 1,
+    slidesPerView: 4,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.rewards-slider-nav.right',
+      prevEl: '.rewards-slider-nav.left',
+    },
+  
+  });
+  
+  const sertificatsSlider = new Swiper('.sertificats-slides', {
+    // Optional parameters
+    loop: true,
+    slidesPerGroup: 1,
+    slidesPerView: 4,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.sertificats-slider-nav.right',
+      prevEl: '.sertificats-slider-nav.left',
+    },
+  
+  });
+  
